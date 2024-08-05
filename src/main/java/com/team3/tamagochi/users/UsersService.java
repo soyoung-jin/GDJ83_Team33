365352dced004011c991ccd631767b963a6b527f
j154370 <lihyo15@gmail.com>
2024-08-05 14:39:48 +0900

    interceptor complete

--- src/main/java/com/team3/tamagochi/users/UsersService.java
+++ src/main/java/com/team3/tamagochi/users/UsersService.java
@@ -15,5 +15,23 @@ public class UsersService {
 		return usersDAO.registerUsers(usersDTO);
 	}
 	
+	public UsersDTO loginUsers(UsersDTO usersDTO) throws Exception{
+		
+		UsersDTO result = usersDAO.loginUsers(usersDTO);
+		
+		// result값이 Null일 경우, 데이터베이스에 존재하지 않는 id를 입력했음을 의미
+		// result가 Null이 아닐 경우, 사용자가 입력한 id값이 데이터베이스에 존재
+		// 따라서 Null이 아닐 경우 password가 맞는지 확인하는 코드 실행
+		if(result != null) {
+			if(result.getUser_pw().equals(usersDTO.getUser_pw())) {
+				return result; // id도 일치하고 password도 일치할 경우 result를 return
+			}else {
+				return null; // id는 일치하지만 password가 일치하지 않을 경우 Null을 리턴
+			}
+		}
+		
+		return null; // id와 password 모두 일치하지 않을 경우 Null을 리턴
+	}
+	
 
 }
