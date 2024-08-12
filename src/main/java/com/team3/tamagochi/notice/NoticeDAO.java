7dfd89bc778e529b6d9e2b84b22277d8b9897587
Soyeong <jthdud0501@gmail.com>
2024-08-12 13:10:17 +0900

    notice소영꺼 에러없음

--- src/main/java/com/team3/tamagochi/notice/NoticeDAO.java
+++ src/main/java/com/team3/tamagochi/notice/NoticeDAO.java
@@ -1,5 +1,21 @@
 package com.team3.tamagochi.notice;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class NoticeDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	private final String NAMESPACE ="com.team3.tamagochi.notice.NoticeDAO.";
+	
+	public List<NoticeDTO> list() throws Exception{
+		return sqlSession.selectList(NAMESPACE + "list");
+	}
 
 }
