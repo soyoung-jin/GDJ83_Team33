a4940b2eb62f0852a8efed2c9f72e5252ec42153
Soyeong <jthdud0501@gmail.com>
2024-08-22 15:15:55 +0900

    rank게시판 하는 중

--- src/main/java/com/team3/tamagochi/rank/RankController.java
+++ src/main/java/com/team3/tamagochi/rank/RankController.java
@@ -2,13 +2,17 @@ package com.team3.tamagochi.rank;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.team3.tamagochi.boards.util.Pager;
+import com.team3.tamagochi.mypet.MyPetDTO;
 
 @Controller
 @RequestMapping(value = "/rank/*")
@@ -19,11 +23,25 @@ public class RankController {
 
 	@GetMapping("rankList")
 	public ModelAndView list(ModelAndView mv, Pager pager) throws Exception {
-		List<RankDTO> ar = rankService.list(pager);
+		List<MyPetDTO> ar = rankService.list(pager);
 		mv.addObject("list", ar);
 		mv.addObject("pager", pager);
 		mv.setViewName("rank/rankList");
+		
 		return mv;
 	}
+	
+
+	@GetMapping("rankDetail")
+	public String detail(MyPetDTO myPetDTO, Model model, HttpSession session, Pager pager) throws Exception{
+			
+		myPetDTO = rankService.detail(myPetDTO);		
+		model.addAttribute("myPetDTO", myPetDTO);
+
+		return "rank/rankDetail";
+
+	}
+	
+	
 
 }
--- src/main/java/com/team3/tamagochi/rank/RankDAO.java
+++ src/main/java/com/team3/tamagochi/rank/RankDAO.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.team3.tamagochi.boards.util.Pager;
+import com.team3.tamagochi.mypet.MyPetDTO;
 
 @Repository
 public class RankDAO {
@@ -16,13 +17,18 @@ public class RankDAO {
 
 	private final String NAMESPACE = "com.team3.tamagochi.rank.RankDAO.";
 
-	public List<RankDTO> list(Pager pager) throws Exception {
+	public List<MyPetDTO> list(Pager pager) throws Exception {
 		return sqlSession.selectList(NAMESPACE + "list", pager);
 	}
 
 	public Long getTotalCount(Pager pager) throws Exception {
 		return sqlSession.selectOne(NAMESPACE + "getTotalCount", pager);
 
+	}
+	
+	public MyPetDTO detail(MyPetDTO myPetDTO) throws Exception{
+		
+		return sqlSession.selectOne(NAMESPACE + "detail", myPetDTO);
 	}
 
 }
